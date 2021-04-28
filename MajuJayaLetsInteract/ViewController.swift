c3fb38c597221553c3df2c00fecd6d12257621cb
aziyan99 <rajaazian08@gmail.com>
2021-04-28 23:24:26 +0700

    make action sheet skeleton

--- MajuJayaLetsInteract/ViewController.swift
+++ MajuJayaLetsInteract/ViewController.swift
@@ -17,7 +17,13 @@ class ViewController: UIViewController {
 
     @IBAction func didTapCountInteractButton(_ sender: Any) {
         let actionSheet = UIAlertController(title: nil, message: "Add interaction count", preferredStyle: .actionSheet)
+        let countButton = UIAlertAction(title: "Wohoo!!!", style: .default)
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel)
         
+        actionSheet.addAction(countButton)
+        actionSheet.addAction(cancelButton)
+        
+        self.present(actionSheet, animated: true, completion: nil)
         
     }
 }
