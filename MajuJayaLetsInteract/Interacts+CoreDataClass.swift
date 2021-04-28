790841f6cec5d73b6917f2defa1efcdbd6c785d1
aziyan99 <rajaazian08@gmail.com>
2021-04-28 22:23:40 +0700

    initial commit

---
+++ MajuJayaLetsInteract/Interacts+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Interacts+CoreDataClass.swift
+//  MajuJayaLetsInteract
+//
+//  Created by Raja Azian on 28/04/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Interacts)
+public class Interacts: NSManagedObject {
+
+}
---
+++ MajuJayaLetsInteract/Interacts+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  Interacts+CoreDataProperties.swift
+//  MajuJayaLetsInteract
+//
+//  Created by Raja Azian on 28/04/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Interacts {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Interacts> {
+        return NSFetchRequest<Interacts>(entityName: "Interacts")
+    }
+
+    @NSManaged public var count: Int16
+    @NSManaged public var name: String?
+    @NSManaged public var reflections: String?
+    @NSManaged public var date: Date?
+
+}
+
+extension Interacts : Identifiable {
+
+}
